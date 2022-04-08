0c58926f3553c8539a2e0fde50d6de07678e2255
Manuel Lang <code@manuel-lang.dev>
2022-04-08 11:52:48 +0200

    chore: add two libraries

---
+++ libs/lib1/src/lib/lib1.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Lib2Module} from 'lib2';
+
+@NgModule({
+  imports: [CommonModule, Lib2Module],
+})
+export class Lib1Module {}
